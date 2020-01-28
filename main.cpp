b2a5b81756a17a5f5916f37eb0fd4dbb4a887c71
cgugas <spartan.1086@gmail.com>
2020-01-27 22:24:38 -0600

    added logo, outlined class structure

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 #include <iostream>
 #include <string>
 
-const std::string helpMsg="\nArguments are \n--daemon or -d to run as daemon\n--help for this menu\n --single or -s for quick test mode\n";
+const std::string helpMsg="\nArguments are \n--daemon or -d to run as daemon\n--help for this menu\n--single or -s for test mode\n";
 
 const std::string noArgs="\nPlease provide argument. See --help\n";
+
+const std::string logo= R"(
+                                         &&             #&.                                        
+                                        &&&&             #&&&&                                      
+                                     &&&&&&&      &/     #&&&&&&                                    
+                                   &&&&&&&&&   ,&&&&&&   %&&&&&&&&&                                 
+                                  &&&&&&&&&  &&&&&&&&&&&.  &&&&&&&&#                                
+                              %&&&  &&&&   &&&&&&(,*&&&&&&&  %&&&  #&&&                             
+                            &&&&&&&&/   &&&&&,   &&&&   &&&&&    &&&&&&&&/                          
+                             &&&&&&&&&&&&&&&     &&&%    &&&&&&&&&&&&&&&(                           
+                               /&&&&&&&&&&&&  &&&& &&&&& #&&&&&&&&&&&&                              
+                              &&  &&&&&&&&#    &&   %&&    &&&&&&&&/ ,&(                            
+                            &&&&&&  *&&&                     %&&&  &&&&&&&                          
+                              &&&&&&&  *&&&               ,&&&  ,&&&&&&                             
+                                ,&&&&&&&&                    &&&&&&&&                               
+                                   &&&                         %&&)";
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +38,7 @@ int main(int argc, char *argv[])
             std::cout<<helpMsg<<std::endl;
             return 1;
         }else if(arg=="--single" || arg=="-s"){
-            std::cout<<"Running in single shot mode"<<std::endl;
+            std::cout<<"Running in non daemon mode"<<std::endl;
         }else{
             std::cout<<"\nUnknown argument. Please see --help\n"<<std::endl;
             return 1;
@@ -28,9 +47,22 @@ int main(int argc, char *argv[])
         std::cout<<noArgs<<std::endl;
         return 1;
     }
-    gpioInitialise();
+
+    if(gpioInitialise()==PI_INIT_FAILED)
+        return 1;
+    std::cout<<logo<<std::endl<<std::endl<<std::endl;
     //set IR pin
+    std::cout<<"Setting pins...\n"<<std::endl;
     gpioSetMode(18, PI_OUTPUT);
     //set source switch pin
+    //
+    //start listeners/interrupts if thats a thing
+    //
+    //run mpd client for screen
+    //if source moves, ir blast classes
+    //
+    //class irblast-> init, set btl, set pi
+    //class mpdclient -> poll mpd, set volume, get volume,play, pause, stop, next, prev, get artist, get album
+    //class display -> print, rgbBacklight, clear, new line
     return 0;
 }
