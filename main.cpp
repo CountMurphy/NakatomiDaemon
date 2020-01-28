372a0a7a2e46bf7a26e334372ebf3a1b3668975c
cgugas <spartan.1086@gmail.com>
2020-01-28 07:57:17 -0600

    reorg strings

--- main.cpp
+++ main.cpp
@@ -2,28 +2,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <string>
-
-const std::string helpMsg="\nArguments are \n--daemon or -d to run as daemon\n--help for this menu\n--single or -s for test mode\n";
-
-const std::string noArgs="\nPlease provide argument. See --help\n";
-
-const std::string logo= R"(
-                                         &&             #&.                                        
-                                        &&&&             #&&&&                                      
-                                     &&&&&&&      &/     #&&&&&&                                    
-                                   &&&&&&&&&   ,&&&&&&   %&&&&&&&&&                                 
-                                  &&&&&&&&&  &&&&&&&&&&&.  &&&&&&&&#                                
-                              %&&&  &&&&   &&&&&&(,*&&&&&&&  %&&&  #&&&                             
-                            &&&&&&&&/   &&&&&,   &&&&   &&&&&    &&&&&&&&/                          
-                             &&&&&&&&&&&&&&&     &&&%    &&&&&&&&&&&&&&&(                           
-                               /&&&&&&&&&&&&  &&&& &&&&& #&&&&&&&&&&&&                              
-                              &&  &&&&&&&&#    &&   %&&    &&&&&&&&/ ,&(                            
-                            &&&&&&  *&&&                     %&&&  &&&&&&&                          
-                              &&&&&&&  *&&&               ,&&&  ,&&&&&&                             
-                                ,&&&&&&&&                    &&&&&&&&                               
-                                   &&&                         %&&)";
-
-
+#include "strings.h"
 
 
 int main(int argc, char *argv[])
---
+++ strings.h
@@ -0,0 +1,22 @@
+#include<string>
+
+const std::string helpMsg="\nArguments are \n--daemon or -d to run as daemon\n--help for this menu\n--single or -s for test mode\n";
+
+const std::string noArgs="\nPlease provide argument. See --help\n";
+
+const std::string logo= R"(
+                                         &&             #&.                                        
+                                        &&&&             #&&&&                                      
+                                     &&&&&&&      &/     #&&&&&&                                    
+                                   &&&&&&&&&   ,&&&&&&   %&&&&&&&&&                                 
+                                  &&&&&&&&&  &&&&&&&&&&&.  &&&&&&&&#                                
+                              %&&&  &&&&   &&&&&&(,*&&&&&&&  %&&&  #&&&                             
+                            &&&&&&&&/   &&&&&,   &&&&   &&&&&    &&&&&&&&/                          
+                             &&&&&&&&&&&&&&&     &&&%    &&&&&&&&&&&&&&&(                           
+                               /&&&&&&&&&&&&  &&&& &&&&& #&&&&&&&&&&&&                              
+                              &&  &&&&&&&&#    &&   %&&    &&&&&&&&/ ,&(                            
+                            &&&&&&  *&&&                     %&&&  &&&&&&&                          
+                              &&&&&&&  *&&&               ,&&&  ,&&&&&&                             
+                                ,&&&&&&&&                    &&&&&&&&                               
+                                   &&&                         %&&)";
+
