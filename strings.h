2df9a021a3df3d6bb4afc2d14a824d004c8873a0
cgugas <spartan.1086@gmail.com>
2020-02-01 13:05:59 -0600

    got blinky

--- strings.h
+++ strings.h
@@ -2,7 +2,7 @@
 
 const std::string helpMsg="\nArguments are \n--daemon or -d to run as daemon\n--help for this menu\n--single or -s for test mode\n";
 
-const std::string noArgs="\nPlease provide argument. See --help\n";
+const std::string noArgs="\nRunning as current user...\n";
 
 const std::string logo= R"(
                                          &&             #&.                                        
@@ -20,3 +20,4 @@ const std::string logo= R"(
                                 ,&&&&&&&&                    &&&&&&&&                               
                                    &&&                         %&&)";
 
+//https://raspberrypi.stackexchange.com/questions/68126/have-the-gpio-pins-changed-between-the-pi-2-and-the-pi-3
