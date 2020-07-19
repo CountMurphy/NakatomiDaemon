bb671da44d3596b170e2071cbcc33b60185193cc
cgugas <spartan.1086@gmail.com>
2020-07-19 16:57:32 -0500

    fixed empty playlist crash

--- mpd.cpp
+++ mpd.cpp
@@ -6,14 +6,22 @@ Mpd::song Mpd::getSongInfo()
     struct mpd_song *msong;
     //mpd_send_list_queue_meta(conn);
     msong = mpd_run_current_song(conn);
-    const char* title = mpd_song_get_tag(msong, MPD_TAG_TITLE, 0);
-    const char* artist = mpd_song_get_tag(msong, MPD_TAG_ARTIST, 0);
-    const char* album = mpd_song_get_tag(msong, MPD_TAG_ALBUM, 0);
-    mpd_response_finish(conn);
     Mpd::song retVal;
-    retVal.title=title == NULL ? "" : title;
-    retVal.artist=artist == NULL ? "" : artist;
-    retVal.album=album == NULL ? "" : album;
+    if(msong == NULL)
+    {
+        // Play list is clear
+        retVal.title = "No Song Info";
+        retVal.album = "";
+        retVal.artist = "";
+    }else{
+        const char* title = mpd_song_get_tag(msong, MPD_TAG_TITLE, 0);
+        const char* artist = mpd_song_get_tag(msong, MPD_TAG_ARTIST, 0);
+        const char* album = mpd_song_get_tag(msong, MPD_TAG_ALBUM, 0);
+        mpd_response_finish(conn);
+        retVal.title=title == NULL ? "" : title;
+        retVal.artist=artist == NULL ? "" : artist;
+        retVal.album=album == NULL ? "" : album;
+    }
     return retVal;
 }
 
@@ -45,6 +53,8 @@ short Mpd::getPlayPercentBlock()
     int pos = mpd_status_get_elapsed_ms(status)/1000;
     struct mpd_song *msong;
     msong = mpd_run_current_song(conn);
+    if(msong == NULL)
+        return 0;
     int length = mpd_song_get_duration(msong);
     mpd_response_finish(conn);
     return (((float)pos/(float)length)*100>=97) ? 20:((int)((float)pos/((float)length)*100))/5;
